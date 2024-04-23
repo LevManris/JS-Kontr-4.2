df4e9ec6a643bcee87b9e461ce28019260c69c7f
Lev Mandris <mandrislev@mail.ru>
2024-04-23 23:42:46 +0300

    first commit

---
+++ src/NotFoundPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      // Заголовок страницы
+      <h2>404 - Not Found</h2>
+      // Сообщение об ошибке
+      <p>The page you're looking for does not exist.</p>
+      // Ссылка на страницу продуктов
+      <Link to="/products">Go to Products</Link>
+    </div>
+  );
+};
+
+export default NotFoundPage;
---
+++ src/ProductsPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import StarRating from './StarRating';
+import './ProductsPage.css';
+
+
+const ProductsPage = ({ products }) => {
+  return (
+    <div className="products-grid">
+      {products.map(product => (
+        <div key={product.id} className="product-card">
+          <img src={product.image} alt={product.title} className="product-image" />
+          <h3>{product.title}</h3>
+          <p>Price: ${product.price}</p>
+          <StarRating rating={product.rating.rate} />
+          <Link to={`/product/${product.id}`}>View Details</Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProductsPage;
